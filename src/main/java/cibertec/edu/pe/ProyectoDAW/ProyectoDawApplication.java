e54e305f252ab8f2bfd1d5d0a2f44b4fbec803d8
chiszo <gigibethandrea@gmail.com>
2023-10-03 12:41:11 -0500

    creando repo y bootstrap

---
+++ src/main/java/cibertec/edu/pe/ProyectoDAW/ProyectoDawApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.ProyectoDAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/ProyectoDAW/ProyectoDawApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.ProyectoDAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
